5213cbce3b58b022ef26b86795b153d36acda6ab
Torsten <torsten@rubydesign.fi>
2023-01-28 23:22:24 +0200

    default member scope to filter unconfirmed

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,8 @@
 class Member < ApplicationRecord
+  def self.default_scope
+    where.not(confirmed_at: nil).where.not(picture: nil)
+  end
+
   # Include default devise modules. Others available are:
   # , :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,:confirmable,
@@ -8,7 +12,7 @@ class Member < ApplicationRecord
   has_many :stories
   has_many :stories
   has_many :pictures
-  
+
   validates :bio, length: { maximum: 1000 }
   validates :name , length: { minimum: 3 }
 
